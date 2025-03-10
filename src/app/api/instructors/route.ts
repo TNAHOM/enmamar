22a510579bc7993001e33ece1528d4fdf0ce3384
Nahom <nahomt2419@gmail.com>
2025-03-10 18:40:01 +0300

    Fix: Fix the api endpoint name change

--- src/app/api/instructors/route.ts
+++ src/app/api/instructors/route.ts
@@ -14,7 +14,7 @@ export async function GET() {
   }
 
   try {
-    const response = await fetch(`${BASEURL}/instructors`, {
+    const response = await fetch(`${BASEURL}/users/instructors/`, {
       method: 'GET',
       headers: {
         'Authorization': `Bearer ${accessToken}`,
